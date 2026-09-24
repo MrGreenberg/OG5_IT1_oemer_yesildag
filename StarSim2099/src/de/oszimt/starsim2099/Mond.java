7434ee4cc6558e0c418d0ab5bcc2164f466cdaa6
Ömer <omeryesildag818@gmail.com>
2026-09-24 10:28:55 +0200

    klasse Mond hinzugefügt

---
+++ StarSim2099/src/de/oszimt/starsim2099/Mond.java
@@ -0,0 +1,48 @@
+package de.oszimt.starsim2099;
+
+public class Mond {
+	private double posX;
+	private double posY;
+	private String art;
+	private String erzart;
+	
+	
+	public double getPosX() {
+		return posX;
+	}
+
+	public void setPosX(double posX) {
+		this.posX = posX;
+	}
+
+	public double getPosY() {
+		return posY;
+	}
+
+	public void setPosY(double posY) {
+		this.posY = posY;
+	}
+
+	public String getArt() {
+		return art;
+	}
+
+	public void setArt(String art) {
+		this.art = art;
+	}
+
+	
+	public String getErzart() {
+		return erzart;
+	}
+
+	public void setErzart(String erzart) {
+		this.erzart = erzart;
+	}
+	
+	
+	
+	
+	
+	
+}
